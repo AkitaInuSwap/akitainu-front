47aae1b470305f5449ff7e3389b58c8725127f59
0xpokotaro <kentarou_masuda@hacklab-tech.io>
2024-04-22 02:28:58 +0900

    ✨ Add add liquidity screen

--- src/pages/liquidity/add/index.tsx
+++ src/pages/liquidity/add/index.tsx
@@ -1,16 +1,28 @@
 'use client'
 
 // React
-import { useRouter } from 'next/router'
+import { useState } from 'react'
+import Image from 'next/image'
 // MUI
+import Box from '@mui/material/Box'
 import Button from '@mui/material/Button'
+import Card from '@mui/material/Card'
+import CardContent from '@mui/material/CardContent'
 import Grid from '@mui/material/Unstable_Grid2'
+import Stepper from '@mui/material/Stepper'
+import Step from '@mui/material/Step'
+import StepLabel from '@mui/material/StepLabel'
+import StepContent from '@mui/material/StepContent'
+import TextField from '@mui/material/TextField'
 import Typography from '@mui/material/Typography'
+import AddIcon from '@mui/icons-material/Add'
 // Components
 import Layout from '@/components/Layout'
 // Types
 import type { ReactElement } from 'react'
 import type { NextPageWithLayout } from '@/pages/_app'
+
+const icon = 'https://assets.pancakeswap.finance/web/native/11155111.png'
 
 const navigations = [
   {
@@ -28,7 +40,15 @@ const navigations = [
 ]
 
 const LiquidityAdd: NextPageWithLayout = () => {
-  const router = useRouter()
+  const [activeStep, setActiveStep] = useState(0)
+
+  const handleNext = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep + 1)
+  }
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1)
+  }
 
   return (
     <Grid container spacing={2}>
@@ -41,6 +61,209 @@ const LiquidityAdd: NextPageWithLayout = () => {
           Select your tokens, enter the amount, and click &apos;Add&apos;.
           You&apos;ll receive LP tokens as proof of your contribution.
         </Typography>
+      </Grid>
+
+      <Grid xs={12}>
+        <Stepper activeStep={activeStep} orientation="vertical">
+          <Step key={1}>
+            <StepLabel>CHOOSE TOKEN PAIR</StepLabel>
+            <StepContent>
+              <Box sx={{ mb: 2 }}>
+                <Typography>hoge</Typography>
+              </Box>
+              <Box sx={{ mb: 2 }}>
+                <Button
+                  variant="contained"
+                  disableElevation
+                  sx={{ mt: 1, mr: 1 }}
+                  onClick={handleNext}
+                >
+                  Next
+                </Button>
+              </Box>
+            </StepContent>
+          </Step>
+
+          <Step key={2}>
+            <StepLabel>DEPOSIT AMOUNT</StepLabel>
+            <StepContent>
+              <Box sx={{ mb: 2 }}>
+                <Typography>hoge</Typography>
+              </Box>
+              <Box sx={{ mb: 2 }}>
+                <Button
+                  variant="outlined"
+                  disableElevation
+                  sx={{ mt: 1, mr: 1 }}
+                  onClick={handleBack}
+                >
+                  Back
+                </Button>
+                <Button
+                  variant="contained"
+                  disableElevation
+                  sx={{ mt: 1, mr: 1 }}
+                  onClick={handleNext}
+                >
+                  Next
+                </Button>
+              </Box>
+            </StepContent>
+          </Step>
+
+          <Step key={3}>
+            <StepLabel>INITIAL FEE</StepLabel>
+            <StepContent>
+            <Box sx={{ mb: 2 }}>
+                <Typography>hoge</Typography>
+              </Box>
+              <Box sx={{ mb: 2 }}>
+                <Button
+                  variant="outlined"
+                  disableElevation
+                  sx={{ mt: 1, mr: 1 }}
+                  onClick={handleBack}
+                >
+                  Back
+                </Button>
+                <Button
+                  variant="contained"
+                  disableElevation
+                  sx={{ mt: 1, mr: 1 }}
+                  onClick={handleNext}
+                >
+                  Next
+                </Button>
+              </Box>
+            </StepContent>
+          </Step>
+
+          <Step key={3}>
+            <StepLabel>CONFIRMATION</StepLabel>
+            <StepContent>
+            <Box sx={{ mb: 2 }}>
+                <Typography>hoge</Typography>
+              </Box>
+              <Box sx={{ mb: 2 }}>
+                <Button
+                  variant="outlined"
+                  disableElevation
+                  sx={{ mt: 1, mr: 1 }}
+                  onClick={handleBack}
+                >
+                  Back
+                </Button>
+                <Button
+                  variant="contained"
+                  disableElevation
+                  sx={{ mt: 1, mr: 1 }}
+                  onClick={handleNext}
+                >
+                  Next
+                </Button>
+              </Box>
+            </StepContent>
+          </Step>
+
+          <Step key={4}>
+            <StepLabel>CONPLETE</StepLabel>
+            <StepContent>
+            <Box sx={{ mb: 2 }}>
+                <Typography>hoge</Typography>
+              </Box>
+              <Box sx={{ mb: 2 }}>
+                <Button
+                  variant="outlined"
+                  disableElevation
+                  sx={{ mt: 1, mr: 1 }}
+                  onClick={handleBack}
+                >
+                  Back
+                </Button>
+                <Button
+                  variant="contained"
+                  disableElevation
+                  sx={{ mt: 1, mr: 1 }}
+                  onClick={handleNext}
+                >
+                  Next
+                </Button>
+              </Box>
+            </StepContent>
+          </Step>
+        </Stepper>
+      </Grid>
+
+      <Grid xs={12}>
+        <Typography component="h6" variant="subtitle1">
+          CHOOSE TOKEN PAIR
+        </Typography>
+      </Grid>
+
+      <Grid xs={12}>
+        <Card variant="outlined">
+          <CardContent>
+            <Box display="flex" alignItems="center">
+              <Image src={icon} alt="icon" width={50} height={50} />
+              <Box>
+                <Typography variant="h6" marginLeft={1}>
+                  SEP
+                </Typography>
+                <Typography variant="body2" marginLeft={1}>
+                  r9iJpeyZTxpYRKZ39L8Dt3MZXRJ7WJtATo
+                </Typography>
+              </Box>
+            </Box>
+          </CardContent>
+        </Card>
+      </Grid>
+
+      <Grid xs={12} sx={{ textAlign: 'center' }}>
+        <AddIcon />
+      </Grid>
+
+      <Grid xs={12}>
+        <Card variant="outlined">
+          <CardContent>
+            <Box display="flex" alignItems="center">
+              <Image src={icon} alt="icon" width={50} height={50} />
+              <Box>
+                <Typography variant="h6" marginLeft={1}>
+                  SEP
+                </Typography>
+                <Typography variant="body2" marginLeft={1}>
+                  r9iJpeyZTxpYRKZ39L8Dt3MZXRJ7WJtATo
+                </Typography>
+              </Box>
+            </Box>
+          </CardContent>
+        </Card>
+      </Grid>
+
+      <Grid xs={12}>
+        <Typography component="h6" variant="subtitle1">
+          DEPOSIT AMOUNT
+        </Typography>
+      </Grid>
+
+      <Grid xs={12}>
+        <Box display="flex" alignItems="center" sx={{ mb: 1 }}>
+          <Image src={icon} alt="icon" width={24} height={24} />
+          <Typography variant="body1" marginLeft={1}>
+            SEP
+          </Typography>
+        </Box>
+        <TextField fullWidth />
+      </Grid>
+
+      <Grid xs={12}>
+        <Box display="flex" alignItems="center" sx={{ mb: 1 }}>
+          <Image src={icon} alt="icon" width={24} height={24} />
+          <Typography variant="body1" marginLeft={1}>
+            SEP
+          </Typography>
+        </Box>
+        <TextField fullWidth />
       </Grid>
     </Grid>
   )
